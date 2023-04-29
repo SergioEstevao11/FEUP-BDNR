016f1a347f847e91da7d8b96b5e06291c44eabe7
Maria Carneiro <mariajcarneiro56@gmail.com>
2023-04-29 02:29:08 +0100

    feat: homepage - missing dropdown on search bars

---
+++ src/app/NetworkOfThrones/src/components/search.tsx
@@ -0,0 +1,26 @@
+import { useState, FormEvent } from 'react';
+
+const Search = (): JSX.Element => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // add your search logic here
+  }
+
+  return (
+    <form onSubmit={handleSearch} className="flex items-center bg-white border border-gray-200 px-3 py-2 shadow-lg rounded-3xl">
+      <input type="text" className="w-full outline-none bg-white text-gray-900 p-2 text-lg placeholder-gray-400" value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button type="submit" className="outline-none focus:outline-none">
+        <svg className="h-5 w-5 text-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+          <path d="M22 22l-6-6"></path>
+          <circle cx="10" cy="10" r="8"></circle>
+        </svg>
+      </button>
+    </form>
+  );
+};
+
+export default Search;
--- src/app/NetworkOfThrones/src/pages/homepage.tsx
+++ src/app/NetworkOfThrones/src/pages/homepage.tsx
@@ -1,15 +1,35 @@
 
 
+import royal from '../assets/royal.jpg';
+import conflict from '../assets/war.jpg';
+
+import Search from '../components/search';
+
 export default function HomePage() {
 
     return (
-      <div>
-        <div id="title" className="flex justify-center items-center">
-          <h1> A </h1>
-          <h1 className="text-janus text-6xl font-bold"> Network </h1>
-          <h1> Of Thrones</h1>
+      <main className='bg-white p-0 sm:mb-20'>
+        <div className='flex-col p-0 sm:m-auto'>
+          <a href='/' className='flex justify-center items-center select-none text-3xl mt-3'>
+            <h1> A </h1>
+            <h1 className="text-janus font-bold"> Network </h1>
+            <h1> Of Thrones</h1>
+          </a>
+          <h3 className="text-2xl my-2">A JanusGraph prototype to analyze kinship and conflicts in Europe</h3>
+
+          <div className="flex w-full justify-around items-center my-5">
+            <div id="search-royals" className="flex flex-col justify-center items-center">
+                <h2 className="text-janus text-5xl font-bold m-5"> Royals </h2>
+                <img className='w-64 h-64 rounded-full object-cover my-5' src={royal} alt="royal" />
+                <Search />
+            </div> 
+            <div id="search-countries" className="flex flex-col justify-center items-center">
+                <h2 className="text-janus text-5xl font-bold m-5"> Conflicts </h2>
+                <img className='w-64 h-64 rounded-full object-cover my-5' src={conflict} alt="conflict" />
+                <Search />
+            </div>
+          </div>
         </div>
-         <h3 className="text-xl my-2">A JanusGraph prototype to analyze kinship and conflicts in Europe</h3>
-      </div> 
+      </main> 
     );
 }
