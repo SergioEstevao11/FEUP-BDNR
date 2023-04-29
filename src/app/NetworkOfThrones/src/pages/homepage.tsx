16f765d97f87172a5297ce1308911fccb08670fd
Maria Carneiro <mariajcarneiro56@gmail.com>
2023-04-29 01:24:32 +0100

    feat: added tailwind

--- src/app/NetworkOfThrones/src/pages/homepage.tsx
+++ src/app/NetworkOfThrones/src/pages/homepage.tsx
@@ -3,8 +3,13 @@
 export default function HomePage() {
 
     return (
-        <>
-        <h1>Home Page</h1>
-        </>
-      ); 
+      <div>
+        <div id="title" className="flex justify-center items-center">
+          <h1> A </h1>
+          <h1 className="text-janus text-6xl font-bold"> Network </h1>
+          <h1> Of Thrones</h1>
+        </div>
+         <h3 className="text-xl my-2">A JanusGraph prototype to analyze kinship and conflicts in Europe</h3>
+      </div> 
+    );
 }
